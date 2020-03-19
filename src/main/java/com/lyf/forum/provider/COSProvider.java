cf63962cf80b15fd774f2c62567959c6719901a7
Liu <1311577860@qq.com>
2020-03-19 10:00:00 +0800

    删除部分

--- src/main/java/com/lyf/forum/provider/COSProvider.java
+++ src/main/java/com/lyf/forum/provider/COSProvider.java
@@ -18,8 +18,7 @@ import java.util.UUID;
 public class COSProvider {
     final Logger logger = LoggerFactory.getLogger(getClass());
     // 1 初始化用户身份信息（secretId, secretKey）。
-    private String secretId = "AKIDPdYFhv3eBVamjRqAC1c4bw3MQFpFokod";
-    private String secretKey = "xf9NN8sQjogKjvsqeHCvQhlatxqUt3yf";
+
     private COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
     // 2 设置 bucket 的区域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
 // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
